7395a3fcb7b016b9ff75fffbdf425f03fafb133c
Seo Jinmann <sjmmics@gmail.com>
2024-05-30 14:10:38 +0900

    상수 클래스 생성   o 게임에 쓰인 숫자의 자리수 등 오타 수정

---
+++ src/main/java/constant/Constant.java
@@ -0,0 +1,15 @@
+package constant;
+
+public class Constant {
+    public static int NUMBER_BALL_PLAY = 3;
+    public static String NO_MATCH_HINT_NAME = "낫싱";
+    public static String MATCH_VALUE_POSITION = "스트라이크";
+    public static String MATCH_VALUE_NO_POSITION = "볼";
+
+    public static String KEEP_PLAYING_FLAG = "1";
+
+    public static String NOT_KEEP_PLAYING_FLAG = "2";
+
+    public static int MIN_BALL_NUMBER = 1;
+    public static int MAX_BALL_NUMBER = 9;
+}
